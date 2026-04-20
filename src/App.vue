138f121e12b97c0b6c9717aa8e5883565ff0dae4
Syed Hassan Raza <105318011+HassanRaza375@users.noreply.github.com>
2026-04-21 00:10:48 +0500

    init hrm

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <v-card height="200">
+              <v-toolbar
+                class="text-white"
+                image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+              >
+                <v-btn icon="mdi-menu"></v-btn>
+
+                <v-toolbar-title text="Toolbar"></v-toolbar-title>
+
+                <v-btn icon="mdi-export"></v-btn>
+              </v-toolbar>
+              <v-btn color="primary">Hello Vuetify!</v-btn>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
